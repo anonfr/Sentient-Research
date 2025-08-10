ee96cc23289ab752ea001f7c25aaae88d7f9c8e9
anonfr <thexo.ogog@gmail.com>
2025-08-10 18:23:48 +0530

    Disable Twitter tool - Discord only deployment

--- src/agent/agent_config.py
+++ src/agent/agent_config.py
@@ -1,4 +1,4 @@
 class AgentConfig:
     def __init__(self):
-        self.TWITTER_ENABLED = True
+        self.TWITTER_ENABLED = False  # Disabled - Discord only
         self.DISCORD_ENABLED = True
